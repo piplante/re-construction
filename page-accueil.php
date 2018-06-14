5373f7a0f5f0850c41171b2e806d1c8b73e9bb2a
Pierre Planté <pi@pierreplante.fr>
2018-06-14 10:50:20 +0200

    no message

--- page-accueil.php
+++ page-accueil.php
@@ -7,9 +7,21 @@
 
 <?php get_header(); ?>
 
-			<div id="content">
+			<div id="hero">
 
-				ACCUEIL
+				<div id="hero-content">
+					<div id="trois-blocs">
+						<div class="blocs-accueil ">
+							bloc1
+						</div>
+						<div class="blocs-accueil ">
+							bloc2
+						</div>
+						<div class="blocs-accueil ">
+							bloc3
+						</div>												
+					</div>
+				</div>
 
 			</div>
 
